ecbc471aea58d2b15892d9e52cf94c4e129f35d5
Nevil Saspara <81670918+nevilsanchpara@users.noreply.github.com>
2024-07-23 09:00:04 -0400

    init

---
+++ script.js
@@ -0,0 +1,89 @@
+const body = document.body
+
+const btnTheme = document.querySelector('.fa-moon')
+const btnHamburger = document.querySelector('.fa-bars')
+
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass)
+  btnTheme.classList.add(btnClass)
+}
+
+const getBodyTheme = localStorage.getItem('portfolio-theme')
+const getBtnTheme = localStorage.getItem('portfolio-btn-theme')
+
+addThemeClass(getBodyTheme, getBtnTheme)
+
+const isDark = () => body.classList.contains('dark')
+
+const setTheme = (bodyClass, btnClass) => {
+
+	body.classList.remove(localStorage.getItem('portfolio-theme'))
+	btnTheme.classList.remove(localStorage.getItem('portfolio-btn-theme'))
+
+  addThemeClass(bodyClass, btnClass)
+
+	localStorage.setItem('portfolio-theme', bodyClass)
+	localStorage.setItem('portfolio-btn-theme', btnClass)
+}
+
+const toggleTheme = () =>
+	isDark() ? setTheme('light', 'fa-moon') : setTheme('dark', 'fa-sun')
+
+btnTheme.addEventListener('click', toggleTheme)
+
+const displayList = () => {
+	const navUl = document.querySelector('.nav__list')
+
+	if (btnHamburger.classList.contains('fa-bars')) {
+		btnHamburger.classList.remove('fa-bars')
+		btnHamburger.classList.add('fa-times')
+		navUl.classList.add('display-nav-list')
+	} else {
+		btnHamburger.classList.remove('fa-times')
+		btnHamburger.classList.add('fa-bars')
+		navUl.classList.remove('display-nav-list')
+	}
+}
+
+btnHamburger.addEventListener('click', displayList)
+
+const scrollUp = () => {
+	const btnScrollTop = document.querySelector('.scroll-top')
+
+	if (
+		body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		btnScrollTop.style.display = 'block'
+	} else {
+		btnScrollTop.style.display = 'none'
+	}
+}
+
+document.addEventListener('scroll', scrollUp)
+
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+	event.preventDefault();
+
+	// Fetch form values
+	let form = this;
+	console.log(form)
+	let formData = {
+		from_name: form.email.value,
+		to_name: 'Athul Tom Mathew', // Replace with recipient's name
+		subject: `new message from ${form.name.value}`,
+		message: form.message.value
+	};
+
+	// Send email
+	emailjs.send(/*your_service_id*/'' ,/*your_template_id*/ '', formData)
+		.then(function(response) {
+			console.log('Email sent!', response);
+			alert('Email sent successfully!');
+			form.reset();
+		}, function(error) {
+			console.error('Error sending email:', error);
+			alert('Error sending email! Please try again later.');
+		});
+});
